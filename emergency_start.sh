eeeb4fa72d55c63cae585aafad9f306d6921440d
Matt Osborne <osbornematt1@gmail.com>
2022-09-25 16:25:28 +1000

    Update emergency_start.sh

--- emergency_start.sh
+++ emergency_start.sh
@@ -3,24 +3,49 @@
 ##Data Drive
 kasa --type strip --host 192.168.20.63 off --index 0
 
+sleep 1
+
 ##TV Land
 kasa --type strip --host 192.168.20.149 off --index 0
+
+sleep 1
+
 kasa --type strip --host 192.168.20.149 off --index 1
+
+sleep 1
+
 kasa --type strip --host 192.168.20.149 off --index 2
+
+sleep 1
 
 ##Bookshelf 1
 
 kasa --type strip --host 192.168.20.64 off --index 0
+
+sleep 1
+
 kasa --type strip --host 192.168.20.64 off --index 1
+
+sleep 1
+
 kasa --type strip --host 192.168.20.64 off --index 2
+
+sleep 1
 
 ##Bookshelf 2
 
 kasa --type strip --host 192.168.20.58 off --index 0
+
+sleep 1
+
 kasa --type strip --host 192.168.20.58 off --index 2
+
+sleep 1
 
 ##Skully Spaceman
 kasa --type strip --host 192.168.20.148 off --index 0
+
+sleep 1
 
 mpg123 ./alarm-buzzer-sound.mp3
 
