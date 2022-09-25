dc4b3d6a261db4e2afdcd8a05dcf072a5a73b2aa
Matt Osborne <osbornematt1@gmail.com>
2022-09-25 15:28:37 +1000

    Move Warning Lights to end

--- emergency_end.sh
+++ emergency_end.sh
@@ -3,10 +3,6 @@
 ##Data Drive
 kasa --type strip --host 192.168.20.63 on --index 0
 
-##Skully Spaceman
-kasa --type strip --host 192.168.20.148 on --index 0
-kasa --type strip --host 192.168.20.148 off --index 1
-
 ##TV Land
 kasa --type strip --host 192.168.20.149 on --index 0
 kasa --type strip --host 192.168.20.149 on --index 1
@@ -24,3 +20,6 @@ kasa --type strip --host 192.168.20.58 on --index 0
 kasa --type strip --host 192.168.20.58 on --index 2
 
 
+##Skully Spaceman
+kasa --type strip --host 192.168.20.148 on --index 0
+kasa --type strip --host 192.168.20.148 off --index 1
--- emergency_start.sh
+++ emergency_start.sh
@@ -3,10 +3,6 @@
 ##Data Drive
 kasa --type strip --host 192.168.20.63 off --index 0
 
-##Skully Spaceman
-kasa --type strip --host 192.168.20.148 off --index 0
-kasa --type strip --host 192.168.20.148 on --index 1
-
 ##TV Land
 kasa --type strip --host 192.168.20.149 off --index 0
 kasa --type strip --host 192.168.20.149 off --index 1
@@ -22,3 +18,7 @@ kasa --type strip --host 192.168.20.64 off --index 2
 
 kasa --type strip --host 192.168.20.58 off --index 0
 kasa --type strip --host 192.168.20.58 off --index 2
+
+##Skully Spaceman
+kasa --type strip --host 192.168.20.148 off --index 0
+kasa --type strip --host 192.168.20.148 on --index 1
